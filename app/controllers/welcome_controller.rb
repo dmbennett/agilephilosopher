8cd2f6869b8aeded1b7b45a6f059de1f0b2cdccc
Dave Bennett <dmbennett@anderson.edu>
2015-02-02 00:36:10 -0500

    derping around with queryparams

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,12 +3,25 @@ class WelcomeController < ApplicationController
     @carousel_photos = ['sloth-money-mind.png', 'sloth-cash-money.jpg','sloth-stripper.jpg','sloth-cigar-spacesuit.jpg','sloth_astronaut.jpg','sloth-floating-space.jpg']
     @carousel_content_counter = 0
     @carousel_photo_counter = 0
-
+    @robby = params[:robby]
   end
 
   def about
+    @robby = params[:robby]
   end
 
   def contact
+    @robby = params[:robby]
+  end
+  private
+  def robby
+    @robby = params[:robby]
+    if @robby
+      session[:robby] = @robby
+    end
+    if @robby
+    else
+      @robby=session[:robby]
+    end
   end
 end
