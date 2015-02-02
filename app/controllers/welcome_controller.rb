03124de54d9102be488cfbe7a813b263764ea6f0
Dave Bennett <dmbennett@anderson.edu>
2015-02-01 19:20:02 -0500

    carousel work

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,7 @@
 class WelcomeController < ApplicationController
   def index
-
+    @carousel_photos = ['sloth-money-mind.png', 'sloth-cash-money.jpg','sloth-stripper.jpg','sloth-cigar-spacesuit.jpg','sloth_astronaut.jpg','sloth-floating-space.jpg']
+    @carousel_count = @carousel_photos.count
   end
 
   def about
