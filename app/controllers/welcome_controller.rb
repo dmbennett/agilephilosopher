4cdefabc470ed5be1149009af5005e5e050202aa
Dave Bennett <dmbennett@anderson.edu>
2015-02-01 20:09:00 -0500

    Fixed the carousel

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,8 @@
 class WelcomeController < ApplicationController
   def index
     @carousel_photos = ['sloth-money-mind.png', 'sloth-cash-money.jpg','sloth-stripper.jpg','sloth-cigar-spacesuit.jpg','sloth_astronaut.jpg','sloth-floating-space.jpg']
-    @carousel_count = @carousel_photos.count
+    @carousel_content_counter = 0
+    @carousel_photo_counter = 0
   end
 
   def about
