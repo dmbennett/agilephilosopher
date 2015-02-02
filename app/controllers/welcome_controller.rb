e0dd2d6d12f00e8897096d200924d2aa72ff1afa
Dave Bennett <dmbennett@anderson.edu>
2015-02-01 22:47:48 -0500

    colors fixed on carousel

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,10 +3,10 @@ class WelcomeController < ApplicationController
     @carousel_photos = ['sloth-money-mind.png', 'sloth-cash-money.jpg','sloth-stripper.jpg','sloth-cigar-spacesuit.jpg','sloth_astronaut.jpg','sloth-floating-space.jpg']
     @carousel_content_counter = 0
     @carousel_photo_counter = 0
+
   end
 
   def about
-
   end
 
   def contact
